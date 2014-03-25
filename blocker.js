3ae8a208ea639294688d4d526d05e752abee307e
Jonathan Eldridge <jonathan@jveldridge.com>
2014-03-25 19:44:03 -0400

    Now only blocks requests that end in .html or .htm

--- blocker.js
+++ blocker.js
@@ -3,29 +3,42 @@
 var block = false;
 var curr = "";
 
+function is_probably_navigation_request(reqUrl) {
+	return hasExtension(reqUrl, ".html") || endsWith(reqUrl, ".htm");
+}
+
+function hasExtension(url, ext) {
+    var stop = url.indexOf('?');
+    if (stop == -1) {
+        stop = url.length;
+    }
+    url = url.substring(0, stop);
+    return endsWith(url, ext);
+}
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
 var callback = function(details) {
-	if (block) {//alert("request happened");
+	if (block && is_probably_navigation_request(details.url)) {
 		console.log("REQUEST SHOULD BE BLOCKED");
-		// console.log(JSON.stringify(details));
-		// console.log(window.location.href);
-		console.log("referrer: " + curr);
+		console.log("request url: " + details.url);
+		console.log("redirecting to: " + curr);
+		block = false;
 		// return {"cancel": true}
 		return {"redirectUrl": curr}
 	}
-	// else {
-	// 	console.log("let request go")
-	// }
 }
 var opt_extraInfoSpec = ["blocking"];
 var filter = {urls: ["<all_urls>"]};
 
-
 chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
 
 var msgReceived = function(message, sender) {
-	console.log("message: " + JSON.stringify(message) + "; sender: " + JSON.stringify(sender));
+	// console.log("message: " + JSON.stringify(message) + "; sender: " + JSON.stringify(sender));
 	curr = sender.url;
-	console.log("curr is now: " + curr);
+	// console.log("curr is now: " + curr);
 	block = true;
 	window.setTimeout(function () {console.log("timer fired"); block = false;}, 1000);
 }
